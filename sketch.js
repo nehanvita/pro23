4cda9d58725e5f3fec75dec261560705b1cd6109
nehanvita <73110611+nehanvita@users.noreply.github.com>
2020-11-03 17:27:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,80 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground;
+var box1,box2,box3;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:-0.1, isStatic:false});
+	World.add(world, packageBody);
+
+
+	
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	box1 = new Box(480,600,20,100)
+	box2 = new Box(340,660,300,20)
+	box3 = new Box(200,600,20,100)
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+  Engine.update(engine)
+
+  box1.display();
+  box2.display();
+  box3.display();
+
+  keyPressed();
+
+  drawSprites();
+
+}
+
+function keyPressed() {
+ if (keyDown(DOWN_ARROW)) {
+    // Look at the hints in the document and understand how to make the package body fall only on
+	packageBody.scale=0.4
+	
+  }
+}
+
